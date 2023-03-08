18f6348d780e3fbeec3cccc3fe98052e93d0dae5
nyesoviktoria <nyeso.viktoria@gmail.com>
2023-03-08 10:02:20 +0100

    collapsibe function works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "bootstrap";
 import "./scss/main.scss";
 import "./scss/variables.scss";
+
+const buttonCollapseElement = document.querySelectorAll(
+  'button[data-id="button-collapse"]'
+);
+
+buttonCollapseElement.forEach((button) =>
+  button.addEventListener("click", () => {
+    button.classList.toggle("rotate-180");
+  })
+);
